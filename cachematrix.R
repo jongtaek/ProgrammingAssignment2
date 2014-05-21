dbfe9ec2be7cbe683c1b8a68f1b5932dd45551a2
jongtaek <jongtaek.park@samsung.com>
2014-05-22 00:12:01 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,14 +5,22 @@
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
+        
+        ## set the value of the matrix
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
         
+        ## get the value of the matrix
         get <- function() x
+        
+        ## set the value of the inverse matrix
         setinverse <- function(sovle) m <<- solve
+        
+        ## get the value of the inverse matrix
         getinverse <- function() m
+        
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
